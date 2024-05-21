abe035a3ab5764eaff447b349fad450eab96f84c
Hatem <hatmramadn@gmail.com>
2024-05-21 13:07:52 +0300

    initial commit

---
+++ App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import {SafeAreaView, Text, View} from 'react-native';
+
+function App(): React.JSX.Element {
+  return (
+    <SafeAreaView>
+      <View>
+        <Text>Product Browser</Text>
+      </View>
+    </SafeAreaView>
+  );
+}
+
+export default App;
